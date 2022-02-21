47905946bfc1338a671b00e323ab19d94d227c82
nikhil <nikhilbadiwal1@gmail.com>
2022-02-21 18:25:04 +0530

    First Commit

---
+++ src/GitPracticeEclipse2/FirstProject.java
@@ -0,0 +1,17 @@
+package GitPracticeEclipse2;
+
+public class FirstProject {
+
+	public static void main(String[] args) {
+		String name="kamal";
+		if(name=="kamal")
+		{
+			System.out.println("name is kamal");
+		}
+		else
+		{
+			System.out.println("name is nikhil");
+		}
+		
+	}
+}
